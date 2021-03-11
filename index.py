4ab82dccea8693b11ce0b6e6b2fe2442029e6780
maagii <mandakh9922@gmail.com>
2021-03-11 22:37:24 +0800

    ppp

---
+++ index.py
@@ -0,0 +1,98 @@
+# print('hello')
+
+varint = 12 #+-*/%
+varstr = "12"
+# print( varint - 13 )
+# print( int(varstr)  - 13)
+varbool = False #< > <=  >= != ==
+# print(str(varbool) + "12")
+# print ( 5 == 8 )    #True, False
+
+aa = ""
+print(aa)
+# if aa == True :
+#     print('aa')
+# if  15>20 :
+#     print('aa')
+# if 25>30 :
+#     print('aa')
+def bb():
+    print('hello')
+    return 'hello'
+
+# if( 6>7 or bb() == "hello"  ):
+#     print('unen garlaa')
+#     print('unen garlaa')
+#     print('unen garlaa')
+# elif 15>20 :
+#     print('hudal garlaa')
+#     print('hudal garlaa')
+#     print('hudal garlaa')
+# elif 25>30 :
+#     print('hudal garlaa')
+#     print('hudal garlaa')
+#     print('hudal garlaa')
+# else :
+#     print('hudal garlaa')
+#     print('hudal garlaa')
+#     print('hudal garlaa')
+# print('bi if-d hamaaralgui')
+
+# x = 10
+# while( x>1 ):
+#     x-=1
+#     if(x%2==1):        
+#         print(x  )
+
+y =20
+z=[6,2,9 , 15, 1, 44,22,66,22]
+# print(z)
+
+# if z[0]>z[1]:
+#     print(z[0])
+# else:
+#     print(z[1])
+# i=0
+# m=0
+# while(i<len(z)):
+    
+#     if z[i]>m:
+#         m=z[i]
+#     i+=1
+# print(m)
+
+k = {'ner': 'bat', 'nas': 19}
+print(k['ner'])
+print(k.get('ner'))
+
+class Person():
+    ner = 'bat'
+    nas = 19
+
+p = Person()
+print(p.ner)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
